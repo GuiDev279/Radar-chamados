1f81a02d01f0857f9b4132601452518a6b130949
guidev279 <guidev279@gmail.com>
2025-08-31 13:40:27 -0300

    adicionada  a página de login

---
+++ src/js/login.js
@@ -0,0 +1,12 @@
+function login(){
+    const usuario = document.getElementById('login').value
+    const senha = document.getElementById('senha').value
+
+    if (usuario === "admin" && senha === "teste"){
+        localStorage.setItem('logado', 'true')
+        window.location.href = "home.html";
+    }else {
+        document.getElementById('erro').textContent = "Usuário ou senha inválidos!";
+    }
+
+}
